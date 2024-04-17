c746574724ea3b36a5642e8a0b9277b2ed183cd8
markussitumorang <markussitumorang1.04@gmail.com>
2024-04-17 19:28:45 +0700

    Update Tugas Pertemuan 7

--- saving_app (Pertemuan 6-7)/saving_app/lib/pages/home/home_page.dart
+++ saving_app (Pertemuan 6-7)/saving_app/lib/pages/home/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/percent_indicator.dart';
+import 'package:flutter/widgets.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 import 'package:saving_app/styles/colors.dart';
 import 'package:saving_app/styles/text_style.dart';
 
@@ -133,8 +135,135 @@ class HomePage extends StatelessWidget {
                 ],
               ),
             ),
+            Container(
+              margin: EdgeInsets.only(top: 150),
+              child: DraggableScrollableSheet(
+                builder: (context, ScrollController) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: kWhite,
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(40),
+                      ),
+                    ),
+                    padding: const EdgeInsets.only(
+                      left: 30,
+                      right: 30,
+                      top: 21,
+                    ),
+                    child: Stack(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 14 + 4),
+                          child: SingleChildScrollView(
+                            controller: ScrollController,
+                            child: Column(
+                              children: [
+                                Center(
+                                  child: Text(
+                                    'Transactions History',
+                                    style: kHeading6.copyWith(
+                                      color: kLuckyBlue,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 31,
+                                ),
+                                _transactionsList(
+                                  kTreeGreen.withOpacity(0.2),
+                                  'assets/icons/up.png',
+                                  'Success!',
+                                  'February 19 03:25 PM',
+                                  '+ 100.000',
+                                ),
+                                _transactionsList(
+                                  kTreeGreen.withOpacity(0.2),
+                                  'assets/icons/up.png',
+                                  'Success!',
+                                  'February 16 03:25 PM',
+                                  '+ 150.000',
+                                ),
+                                _transactionsList(
+                                  kOrange.withOpacity(0.2),
+                                  'assets/icons/down.png',
+                                  'Coffe Shop',
+                                  'February 10 03:25 PM',
+                                  '- 110.000',
+                                ),
+                                _transactionsList(
+                                  kOrange.withOpacity(0.2),
+                                  'assets/icons/down.png',
+                                  'Payment Invest',
+                                  'February 1 03:25 PM',
+                                  '- 150.000',
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.topCenter,
+                          child: Container(
+                            height: 4,
+                            width: 49,
+                            color: kEgyptianBlue.withOpacity(0.1),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            )
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _transactionsList(
+      Color bgColor, String icon, String title, String sub, String amount) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        bottom: 30,
+      ),
+      child: Row(
+        children: [
+          SizedBox(
+            height: 30,
+            width: 30,
+            child: CircleAvatar(
+              backgroundColor: bgColor,
+              child: Image(
+                image: AssetImage(icon),
+                width: 14,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: kBody1.copyWith(color: kLuckyBlue),
+              ),
+              Text(
+                sub,
+                style: kCaption.copyWith(color: kLightGray),
+              ),
+            ],
+          ),
+          Spacer(),
+          Text(
+            amount,
+            style: kBody1.copyWith(color: kLuckyBlue),
+          )
+        ],
       ),
     );
   }
